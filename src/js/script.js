fd1f61cce1e2f8ff45ed8073cd785439aec094e0
Inaldo Macena <inaldomacena19@gmail.com>
2023-07-11 00:44:02 -0300

    Construção do codigo Javascript

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,48 @@
 
 
 // Objetivo 1- quando passar o mouse em cima do personagem, devemos seleciona-lo
-/* passo 1- pegar os personagens no JS para poder verificar quando o usuario passar o mouse em cima de um deles*/
+/* Passo 1- pegar os personagens no JS para poder verificar quando o usuario passar o mouse em cima de um deles*/
  
- document.querySelectorAll('.personagem')
+  const  personagens = document.querySelectorAll('.personagem');
+
+  // Passo 2 - adicionar a classe selecionado no personagemque o usuario passar o cursos do mouse 
+
+  personagens.forEach((personagem) => {
+          personagem.addEventListener('mouseenter', () => {
+            
+
+//Passo 3 - verificar e já  exista um personagem elecionado, se sim, devemos remover a seleção dele
+
+        const personagemSelecionado = document.querySelector('.selecionado');
+        personagemSelecionado.classList.remove('selecionado')
+
+        
+
+        personagem.classList.add('selecionado');
+
+
+
+// Objetivo 2 - Quando passar o mouse em cima do personagem na listagem, trocar a imagem, o nome e a descrição do personagem grande
+
+// Passo 1 - Pegar o elemento do personagem grande para adicionar as informações nele
+
+
+        const imagemPersonagemGrande = document.querySelector('.personagem-selecionado');
+        
+// Passo 2 - Alterar  a imagem  do personagem grande
+        const idPersonagem = personagem.attributes.id.value;
+        imagemPersonagemGrande.src =`/src/imagens/card-${idPersonagem}.png`;
+
+//Passo 3 - Alterar o nome do personagem grande
+
+         const nomePersonagem = document.getElementById('nome-personagem');
+
+         nomePersonagem.innerText = personagem.getAttribute('data-name');
+
+
+          } )
+  })
+
+  
+
+  
