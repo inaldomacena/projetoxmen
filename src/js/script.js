ad10c371893bb785a04f88e7743092dd01610bea
Inaldo Macena <inaldomacena19@gmail.com>
2023-07-10 23:26:41 -0300

    criando arquivo js

---
+++ src/js/script.js
@@ -0,0 +1,6 @@
+
+
+// Objetivo 1- quando passar o mouse em cima do personagem, devemos seleciona-lo
+/* passo 1- pegar os personagens no JS para poder verificar quando o usuario passar o mouse em cima de um deles*/
+ 
+ document.querySelectorAll('.personagem')
